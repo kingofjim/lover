380009317e08a2c4d3f42d5774c4633b4616ce33
Jim Chang <bill3975598@gmail.com>
2020-05-30 15:26:35 +0800

    modify model

--- app/models.py
+++ app/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Players(models.Model):
     name = models.CharField(max_length=8)
-    nickname = models.CharField(max_length=15, null=True)
+    nickname = models.CharField(max_length=15, null=True, blank=True)
     phone = models.CharField(max_length=15)
     email = models.CharField(max_length=100)
     birthday = models.DateField()
     line = models.CharField(max_length=100)
-    ig = models.CharField(max_length=100, null=True)
-    fb = models.CharField(max_length=100, null=True)
+    ig = models.CharField(max_length=100, null=True, blank=True)
+    fb = models.CharField(max_length=100, null=True, blank=True)
     photo_whole = models.CharField(max_length=150, null=True)
     photo_half = models.CharField(max_length=150, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
